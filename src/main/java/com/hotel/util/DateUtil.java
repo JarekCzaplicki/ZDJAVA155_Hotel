906d946da0cb758782fb48225fdd1a5f173790df
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-06 17:36:49 +0200

    Dodanie inicjalizacji danymi

--- src/main/java/com/hotel/util/DateUtil.java
+++ src/main/java/com/hotel/util/DateUtil.java
@@ -1,5 +1,6 @@
 package com.hotel.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
@@ -8,7 +9,12 @@ import java.util.Date;
 
 @Component
 public class DateUtil {
+    @Value("${date.jarek}")
+    String date;
     public Date createDateFromString(String dateStr) {
+        System.out.println("*************************");
+        System.out.println(date);
+
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date;
         try {
